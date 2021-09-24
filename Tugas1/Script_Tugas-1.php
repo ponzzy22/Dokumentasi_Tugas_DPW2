6d3e3d82582c27159613fa6baa06c9313f40cea7
Ponsians Jopi <90673400+ponzzy22@users.noreply.github.com>
2021-09-24 12:25:11 +0700

    Tugas1

---
+++ Tugas1/Script_Tugas-1.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "Momo Adalah Kucing<br>";
+echo "Punya Kaki Sebanyak : 4<br>";
+echo "Tidak Bisa Terbang<br>";
+echo "Suaranya : meooong";
+
+echo "<hr>";
+
+echo "Doggy Adalah Anjing<br>";
+echo "Punya Kaki Sebanyak : 4<br>";
+echo "Tidak Bisa Terbang<br>";
+echo "Suaranya : guk guk";
+
+echo "<hr>";
+
+echo "Zya Adalah Elang<br>";
+echo "Punya Kaki Sebanyak : 2<br>";
+echo "Bisa Terbang<br>";
+echo "Suaranya : wik wik";
+
+echo "<hr>";
+
+echo "Masha Adalah Angsa<br>";
+echo "Punya Kaki Sebanyak : 2<br>";
+echo "Bisa Terbang<br>";
+echo "Suaranya : kweeek";
+
+echo "<hr>";
+?>
